b5a1daecc1df4aca41b658cc351c4fb908e35ae7
Yogesh <me.yogesh.pal@gmail.com>
2023-06-14 23:00:54 +0530

    Employee wage problem

---
+++ EmpWage/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.Design;
+
+namespace EmpWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int emp_full_time_present = 1;
+            int empHrs = 0;
+            int empWage = 0;
+            int emp_Rate_per_hrs = 20;
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == emp_full_time_present)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+
+            empWage = empHrs * emp_Rate_per_hrs;
+            Console.WriteLine("Employee Wage: " + empWage);
+        }
+    }
+}
