ab8b1a5d18e1caf4359b8d897abb1f829ec757d5
Yogesh <me.yogesh.pal@gmail.com>
2023-06-14 22:50:31 +0530

    Employee wage using class method

---
+++ EmpWgClassMet/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace EmpWgClassMet
+{
+    internal class Program
+    {
+        int Monthly_wage;
+        const int Is_full_time = 2;
+        const int Is_part_time = 1;
+        int Emp_rate_per_hrs = 20;
+        int Total_working_days = 2; 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
