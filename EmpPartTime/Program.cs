de410ace185e9712de4a579990b63dabc3195d21
Yogesh <me.yogesh.pal@gmail.com>
2023-06-14 22:56:45 +0530

    Employee wage Part Time

---
+++ EmpPartTime/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace EmpPartTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int EmpWage = 0;
+            int EmpHrs = 0;
+            int Emp_rate_per_hrs = 20;
+            int Emp_part_time_present = 1;
+            int Emp_full_time_presnt = 2;
+            Random random = new Random();
+            int EmpCheck = random.Next(0, 3);
+
+            if (EmpCheck == Emp_part_time_present)
+            {
+                EmpHrs = 4;
+            }
+
+            else if (EmpCheck == Emp_full_time_presnt)
+            {
+                EmpHrs = 8;
+            }
+
+            else
+            {
+
+                EmpHrs = 0;
+            }
+
+            EmpWage = EmpHrs * Emp_rate_per_hrs;
+            Console.WriteLine("Employee Wage is " + EmpWage);
+
+        }
+    }
+}
